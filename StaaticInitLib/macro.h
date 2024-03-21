2cea58c89075136b1ca880c64ec6989e76f7d249
Unchpokable <Unchpokable.official@gmail.com>
2024-03-22 03:29:20 +0500

    removed unused code

--- StaaticInitLib/macro.h
+++ StaaticInitLib/macro.h
@@ -1,18 +1,5 @@
 #pragma once
 
-//#define STATIC_CONSTRUCTOR(className, priority) \
-//    static void className##_StaticInit(); \
-//    class className##_Init \
-//    { \
-//    public: \
-//        className##_Init() \
-//        { \
-//            std::cout << "Macro Debug: Initialization of " << #className << "\n"; \
-//            StaticInit::addInitFunction(className##_StaticInit, priority); \
-//        }\
-//    }; \
-//    static className##_Init className##_init;
-
 #define STATIC_CONSTRUCTOR(className, priority) \
     static void StaticInit(); \
     private: \
